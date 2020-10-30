0ae72a967f60f838d926e2a7749e4ea0c07f9fde
mostafa elspagh <mostafaelspagh0@gmail.com>
2020-10-30 11:14:33 +0200

    upload

---
+++ codeforces/A. Beautiful Matrix/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 5; j++) {
+      cin >> t;
+      if (t == 1) {
+        cout << abs(i - 3) + abs(j - 3);
+        return 0;
+      }
+    }
+  }
+}
---
+++ codeforces/A. Team/solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ans  = 0 ;
+    int a , b , c;
+    for(int i = 0 ;i < n; i++){
+        cin>>a>>b>>c;
+        if(a + b + c >= 2) ans += 1;
+    }
+    cout<<ans;
+}
